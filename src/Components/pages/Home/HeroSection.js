ef44e6d145301c7914e217338285a9d38c63f779
AshishDhimal <yonyangkamal11@gmail.com>
2023-09-09 13:28:23 +1000

    @u@

--- src/Components/pages/Home/HeroSection.js
+++ src/Components/pages/Home/HeroSection.js
@@ -1,36 +1,129 @@
 import React from "react";
 import a from "../../../assets/a.jpg";
+import b from "../../../assets/b.jpg";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import videoBackground from "../../../assets/video1.mp4";
 const HeroSection = () => {
   const products = [
     {
       id: 1,
       name: "Product 1",
       price: 19.99,
-      imageUrl: { a },
+      imageUrl: a,
     },
     {
       id: 2,
       name: "Product 2",
       price: 29.99,
-      imageUrl: "/product2.jpg",
+      imageUrl: b,
+    },
+    {
+      id: 2,
+      name: "Product 3",
+      price: 29.99,
+      imageUrl: a,
+    },
+    {
+      id: 2,
+      name: "Product 4",
+      price: 29.99,
+      imageUrl: b,
+    },
+    {
+      id: 2,
+      name: "Product 5",
+      price: 29.99,
+      imageUrl: a,
+    },
+    {
+      id: 2,
+      name: "Product 6",
+      price: 29.99,
+      imageUrl: b,
     },
   ];
   return (
     <div>
       {/* Hero Section */}
-      <section className="bg-gray-900 text-white py-16">
-        <div className="container mx-auto text-center">
+      <section className="bg-gray-900 text-white py-16 relative">
+        <div className=" absolute top-0 left-0 w-full h-full overflow-hidden">
+          <video
+            autoPlay
+            muted
+            loop
+            id="video-bg"
+            className="object-cover w-full h-full"
+          >
+            <source src={videoBackground} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="container mx-auto text-center relative z-10">
           <h1 className="text-4xl font-extrabold mb-4">
             Welcome to Your E-Commerce Store
           </h1>
           <p className="text-lg mb-8">
             Discover the best deals on our products.
           </p>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full text-lg">
+          <button className="bg-red-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full text-lg">
             Shop Now
           </button>
+        </div>
+      </section>
+
+      {/* Delivery Info  */}
+
+      <section className="bg-grey-100 py-20">
+        <div className="container mx-auto text-center">
+          <div className="flex flex-wrap justify-center items-stretch">
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-4">
+              <div className="bg-white p-4 rounded-lg shadow-lg flex flex-col justify-between h-full">
+                <div>
+                  <div className="text-xl text-grey-600  m-6">
+                    <i className="fas fa-truck mr-2"></i>
+                  </div>
+                  <h5>Fast Delivery</h5>
+                  <p className="text-gray-700">Starts from $100</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-4">
+              <div className="bg-white p-4 rounded-lg shadow-lg flex flex-col justify-between h-full">
+                <div>
+                  <div className="text-xl text-600 m-6">
+                    <i className="fas fa-money-bill-wave mr-2"></i>
+                  </div>
+                  <h5>Money Back</h5>
+                  <p className="text-gray-700">7 Days Back</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-4">
+              <div className="bg-white p-4 rounded-lg shadow-lg flex flex-col justify-between h-full">
+                <div>
+                  <div className="text-xl text--600 m-6">
+                    <i className="fas fa-lock mr-2"></i>
+                  </div>
+                  <h5>Payment</h5>
+                  <p className="text-gray-700">Secure System</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-4">
+              <div className="bg-white p-4 rounded-lg shadow-lg flex flex-col justify-between h-full">
+                <div>
+                  <div className="text-xl text-600  m-6">
+                    <i className="fas fa-undo mr-2"></i>
+                  </div>
+                  <h5>365 Days</h5>
+                  <p className="text-gray-700">For free return</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
--- src/Components/pages/Home/HomeSection.js
+++ src/Components/pages/Home/HomeSection.js
@@ -1,55 +1,87 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 
-const Sidebar = () => {
+const HomeSection = () => {
   const products = [
     {
       id: 1,
       name: "Product 1",
       price: 19.99,
       imageUrl: "/product1.jpg",
-      category: "Category A",
+      category: "Guitars",
     },
     {
       id: 2,
       name: "Product 2",
       price: 29.99,
       imageUrl: "/product2.jpg",
-      category: "Category B",
+      category: "Amps",
     },
     {
       id: 2,
       name: "Product 2",
       price: 29.99,
       imageUrl: "/product2.jpg",
-      category: "Category C",
+      category: "Pedals & FX",
     },
     {
       id: 3,
       name: "Product 3",
       price: 29.99,
       imageUrl: "/product2.jpg",
-      category: "Category D",
+      category: "Studio",
     },
     {
       id: 4,
       name: "Product 4",
       price: 29.99,
       imageUrl: "/product2.jpg",
-      category: "Category B",
+      category: "PA/Live",
     },
+
+    {
+      id: 4,
+      name: "Product 4",
+      price: 29.99,
+      imageUrl: "/product2.jpg",
+      category: "Keyboards & Synths",
+    },
+
+    {
+      id: 4,
+      name: "Product 4",
+      price: 29.99,
+      imageUrl: "/product2.jpg",
+      category: "Accessories",
+    },
+
+    {
+      id: 4,
+      name: "Product 4",
+      price: 29.99,
+      imageUrl: "/product2.jpg",
+      category: "Pre-Owned",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 29.99,
+      imageUrl: "/product2.jpg",
+      category: "Specials",
+    },
+
     // Add more product objects as needed
   ];
   const categories = [...new Set(products.map((product) => product.category))];
   return (
     <div className="flex">
       {/* Sidebar */}
-      <aside className="w-1/4 bg-gray-200 p-4">
-        <h2 className="text-xl font-semibold mb-4">Product Categories</h2>
+      <aside className="w-1/4 h-100 bg-gray-200 p-4 mt-4">
+        <h2 className="text-xl font-semibold m-6">Product Categories</h2>
         <ul>
           {categories.map((category) => (
             <li key={category} className="mb-2">
-              <a href="#" className="text-blue-500 hover:underline">
+              <a href="#" className="text-black-500 hover:underline">
                 {category}
               </a>
             </li>
@@ -64,4 +96,4 @@ const Sidebar = () => {
   );
 };
 
-export default Sidebar;
+export default HomeSection;
